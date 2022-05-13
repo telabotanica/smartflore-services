5e69cf43ef228ff2f0c5f66b55f04c709485e732
kstefanini <killian@stefanini.fr>
2022-05-13 16:21:17 +0200

    Add refresh command

---
+++ src/Command/CacheRefreshCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\CacheService;
+use App\Service\TrailsService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CacheRefreshCommand extends Command
+{
+    protected static $defaultName = 'app:cache:refresh';
+    protected static $defaultDescription = 'Add a short description for your command';
+
+    private $cache;
+    private $trails;
+
+    public function __construct(
+        string $name = null,
+        CacheService $cache,
+        TrailsService $trails
+    ) {
+        $this->cache = $cache;
+        $this->trails = $trails;
+
+        parent::__construct($name);
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('resource', InputArgument::OPTIONAL, 'Which cache should we refresh?', 'none')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $resource = $input->getArgument('resource');
+
+        if ('none' === $resource) {
+            $io->note(sprintf('Specify resource to refresh:'));
+            $io->note(sprintf(' - all (trails and cards, can take long)'));
+            $io->note(sprintf(' - trails (trails list)'));
+            $io->note(sprintf(' - <trail-name>'));
+        }
+
+        switch ($resource) {
+            case 'all':
+                $this->cache->refresh();
+                break;
+            case 'trails':
+                $this->trails->getTrails(true);
+                break;
+            default:
+                $this->trails->getTrail($resource, true);
+        }
+
+        $io->success('OK! '.$resource.' cache refreshed successfully :)');
+
+        return Command::SUCCESS;
+    }
+}
