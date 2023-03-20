1771736f6a426f735ce8e4f57b27b3970138e926
Julien MARTIN <ju.martin@outlook.com>
2023-03-20 14:15:51 +0100

    update response for /me route

--- src/Model/Trail.php
+++ src/Model/Trail.php
@@ -37,7 +37,7 @@ class Trail
      *     type="string",
      *     example="Arbres vraiment remarquables mais genre de ouf tmtc"
      * )
-     * @Groups({"show_trail", "list_trail"})
+     * @Groups({"show_trail", "list_trail", "user_trail"})
      * @SerializedName("display_name")
      */
     private $displayName;
@@ -48,7 +48,7 @@ class Trail
      *     type="string",
      *     example="Tela Botanica"
      * )
-     * @Groups({"show_trail", "list_trail"})
+     * @Groups({"show_trail", "list_trail", "user_trail"})
      * @SerializedName("author")
      */
     private $auteur;
@@ -64,7 +64,6 @@ class Trail
      *     type="string",
      *     example="draft"
      * )
-     * @Groups({"user_trail"})
      */
     private $status;
 
@@ -75,7 +74,7 @@ class Trail
      *     @OA\Items(type="array", @OA\Items(type="float")),
      *     example={"start":{"lat":43.610769, "lon":3.876716}, "end":{"lat":43.610769, "lon":3.876716}}
      * )
-     * @Groups({"show_trail", "list_trail"})
+     * @Groups({"show_trail", "list_trail", "user_trail"})
      */
     private $position;
 
@@ -95,7 +94,7 @@ class Trail
      *     type="int",
      *     example="42"
      * )
-     * @Groups({"show_trail", "list_trail"})
+     * @Groups({"show_trail", "list_trail", "user_trail"})
      */
     private $occurrencesCount;
 
@@ -105,14 +104,14 @@ class Trail
      *     type="string",
      *     example="https://example.com/link+to+trail+details"
      * )
-     * @Groups({"list_trail"})
+     * @Groups({"list_trail", "user_trail"})
      */
     private $details;
 
     /**
      * @var ?Image
      * @OA\Property(ref=@Model(type=Image::class))
-     * @Groups({"show_trail", "list_trail"})
+     * @Groups({"show_trail", "list_trail", "user_trail"})
      */
     private $image;
 
@@ -130,7 +129,7 @@ class Trail
      *     type="int",
      *     example="420"
      * )
-     * @Groups({"show_trail", "list_trail"})
+     * @Groups({"show_trail", "list_trail", "user_trail"})
      */
     private $pathLength;
 
