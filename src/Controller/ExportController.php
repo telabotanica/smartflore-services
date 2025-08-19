0598df4908a62099424e080d2f18f6b15aae7a75
Julien MARTIN <ju.martin@outlook.com>
2025-08-19 10:53:26 +0200

    add export trails in csv

---
+++ src/Controller/ExportController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SentierRepository;
+use App\Service\AnnuaireService;
+use App\Service\ExportService;
+use OpenApi\Annotations as OA;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExportController extends AbstractController
+{
+    private AnnuaireService $annuaire;
+    private ExportService $exportService;
+    private SentierRepository $sentierRepository;
+
+    public function __construct(AnnuaireService $annuaire, ExportService $exportService, SentierRepository $sentierRepository)
+    {
+        $this->annuaire = $annuaire;
+        $this->exportService = $exportService;
+        $this->sentierRepository = $sentierRepository;
+    }
+
+    /**
+     * @OA\Response(
+     *     response="200",
+     *     description="Liste des sentiers généré en csv"
+     * )
+     * @OA\Tag(name="Export")
+     * @OA\Get(
+     *     summary="Generate a csv file with all trails (admins only)",
+     * )
+     * @Route("/export/csv", name="export_csv", methods={"GET"})
+     */
+    public function export_csv(Request $request): Response
+    {
+        ['user' => $user, 'token'=> $token, 'error' => $error] = $this->annuaire->getUserFromRequest($request);
+        if ($error) {
+            return new JsonResponse(['error' => $error], Response::HTTP_UNAUTHORIZED);
+        }
+        if (!$user || !$token) {
+            return new JsonResponse(['error' => 'Erreur d\'authentification, veuillez vous reconnecter'], Response::HTTP_UNAUTHORIZED);
+        }
+        if (!$this->annuaire->isAdmin($user)) {
+            return new JsonResponse(['error' => 'You need to be an administrator to display this list of trails'], Response::HTTP_FORBIDDEN);
+        }
+
+        $list = $this->sentierRepository->findBy(['date_suppression' => null]);
+
+        $response = $this->exportService->exportTrailsCsv($list);
+        $response->headers->set('Content-Type', 'text/csv; charset=UTF-8');
+
+        return $response;
+    }
+}
---
+++ src/Service/ExportService.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ExportService
+{
+    public function createCsv(array $list, string $fileName): Response
+    {
+        $fp = fopen('php://temp', 'w');
+        foreach ($list as $fields) {
+            fputcsv($fp, $fields);
+        }
+
+        rewind($fp);
+        $response = new Response(stream_get_contents($fp));
+        fclose($fp);
+
+        $response->headers->set('Content-Type', 'text/csv; charset=UTF-8');
+        $response->headers->set('Content-Disposition', 'attachment; filename=' . $fileName . '.csv');
+
+        return $response;
+    }
+
+    public function exportTrailsCsv(array $sentiers) {
+        $list = [[
+            'id',
+            'titre',
+            'auteurId',
+            'auteur',
+            'email',
+            'dateCreation',
+            'dateDerniereModif',
+            'etat',
+            'pmr',
+            'meilleuresSaisons',
+            "details",
+            "nbTaxons",
+            "nbOccurences",
+        ]];
+
+        foreach ($sentiers as $sentier) {
+            $dateCreation = $sentier->getDateCreation()->format('Y/m/d H:i:s');
+            $dateDerniereModif = $sentier->getDateModification() ? $sentier->getDateModification()->format('Y/m/d H:i:s') : null;
+
+            $list[] = [
+                $sentier->getId(),
+                $sentier->getNom(),
+                $sentier->getAuthorId(),
+                $sentier->getAuteur(),
+                $sentier->getAuteurEmail(),
+                $dateCreation,
+                $dateDerniereModif,
+                $sentier->getStatus(),
+                $sentier->getPmr(),
+                is_array($sentier->getMeilleuresSaisons()) ? implode(',', $sentier->getMeilleuresSaisons()) : $sentier->getMeilleuresSaisons(),
+                $sentier->getDetails(),
+                $sentier->getNbTaxons(),
+                $sentier->getOccurrencesCount()
+            ];
+        }
+
+        $today = (new \DateTime())->format('YmdHis');
+
+        return $this->createCsv($list, 'export_sentiers_'. $today);
+    }
+}
