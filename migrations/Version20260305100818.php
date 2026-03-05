e0fe330a92d0685d7d9cd8c7b8fb50461ec661fd
Julien MARTIN <ju.martin@outlook.com>
2026-03-05 13:28:36 +0100

    update migration

--- migrations/Version20260305100818.php
+++ migrations/Version20260305100818.php
@@ -20,14 +20,12 @@ final class Version20260305100818 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE eFloreRedaction_triples');
         $this->addSql('ALTER TABLE ping ADD from_website TINYINT(1) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE eFloreRedaction_triples (id INT UNSIGNED AUTO_INCREMENT NOT NULL, resource VARCHAR(255) CHARACTER SET latin1 DEFAULT \'\' NOT NULL COLLATE `latin1_swedish_ci`, property VARCHAR(255) CHARACTER SET latin1 DEFAULT \'\' NOT NULL COLLATE `latin1_swedish_ci`, value TEXT CHARACTER SET latin1 NOT NULL COLLATE `latin1_swedish_ci`, INDEX property (property), INDEX resource (resource), PRIMARY KEY(id)) DEFAULT CHARACTER SET latin1 COLLATE `latin1_swedish_ci` ENGINE = MyISAM COMMENT = \'\' ');
         $this->addSql('ALTER TABLE ping DROP from_website');
     }
 }
